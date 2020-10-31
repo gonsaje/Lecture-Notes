7420684603a7fbebdc2383a8e435195ed9eac3f3
gonsaje <song.jaehyun@gmail.com>
2020-10-30 20:01:06 -0400

    W1D4 hw instantiated

---
+++ W1D4_hw.js
@@ -0,0 +1,126 @@
+// logEach
+// Write a function `logEach(array)` that prints every element of the array and its
+// index to the console.
+//
+
+function logEach() {
+
+}
+
+// Example:
+//
+// logEach(["Anthony", "John", "Carson"]); // prints
+// 0: Anthony
+// 1: John
+// 2: Carson
+
+///////////////////////////////////////
+// range
+// Write a function range(start, end) that returns an array that contains all
+// numbers between 'start' and 'end' in sequential order.
+//
+
+function range(start, end) {
+
+}
+
+// Examples:
+//
+// console.log(range(1,4)) // => [1,2,3,4]
+// console.log(range(4,2)) => []
+
+///////////////////////////////////
+
+// sumArray
+// Write a function sumArray(array) that takes in an array of numbers that returns the
+// total sum of them.
+//
+//
+
+function sumArray(array) {
+
+}
+
+// Examples:
+//
+// console.log(sumArray([5, 6, 4])); // => 15
+// console.log(sumArray([7, 3, 9, 11])); // => 30
+
+/////////////////////////////////////
+
+// capWords
+// Write a function `capWords(words)` that takes in an array of words and
+// returns a new array where every word is capitalized.
+//
+
+function capWords(words) {
+
+}
+
+// Example:
+//
+// console.log(capWords(['hello', 'boOtCaMp', 'PREP!'])); // => ['HELLO', 'BOOTCAMP', 'PREP!']
+
+/////////////////////////////////////
+
+// wordPeriods
+// Write a function `wordPeriods(sentence)` that takes in a sentence and returns a new
+// sentence where every word has period after it.
+//
+
+function wordPeriods(sentence) {
+
+}
+
+// Examples:
+//
+// console.log(wordPeriods('hello world')); // => 'hello. world.'
+// console.log(wordPeriods('what is the weather today')); // => 'what. is. the. weather. today.'
+
+///////////////////////////////////////
+
+// maxValue
+// Write a function `maxValue(array)` that returns the largest value in `array`.
+// Assume `array` is an array of numbers.
+//
+
+function maxValue(array) {
+
+}
+
+// Examples:
+//
+// console.log(maxValue([12, 6, 43, 2])); // => 43
+// console.log(maxValue([])); // => null
+// console.log(maxValue([-4, -10, 0.43])); // => 0.43
+
+////////////////////////////////////////////
+
+// myIndexOf
+// Write a function `myIndexOf(array, target)` that takes in an array of
+// numbers and a target number as arguments. It should return the index value
+// of the target if it is present in the array or -1 if it is not present.
+//
+// CONSTRAINT: Do not use the indexOf method.
+//
+
+function myIndexOf(array, target) {
+
+}
+
+// Examples:
+//
+// console.log(myIndexOf([1,2,3,4],4)) => 3
+// console.log(myIndexOf([5,6,7,8],2)) => -1
+
+//////////////////////////////////
+
+// factorArray
+// Write a function `factorArray(array, number)` that takes in an array of
+// numbers and a number and returns an array of all the factors.
+//
+// Examples:
+//
+// console.log(factorArray([2,3,4,5,6],20)) // => [2,4,5]
+// console.log(factorArray([2,3,4,5,6],35)) // => [5]
+// console.log(factorArray([10,15,20,25],5)) // => []
