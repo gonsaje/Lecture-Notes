1e0ccd206a7759668ae65ab1200639ac81cae1ee
gonsaje <song.jaehyun@gmail.com>
2020-11-23 23:11:31 -0500

    updated w1d1hw

--- w1d1_hw.js
+++ w1d1_hw.js
@@ -64,109 +64,109 @@ true && !(false || false)
 
 
 
-// 1
-var idx = 'abcde'.indexOf('D');
-idx = idx + 11;
-console.log(idx); // ?
-idx * 2;
-console.log(idx); // ?
-// 2
-var num = 33;
-var isEven = num % 2 === 0;
-console.log(isEven); // ?
-console.log(!isEven); // ?
-// 3
-var str1 = 'marker';
-var num = 'whiteboard'.length - str1.length;
-console.log(num); // ?
-var str2 = 'bootcamp';
-console.log(str2[num].toUpperCase()); // ?
-var char = str2[num].toLowerCase(); // ?
-console.log(char + '!'); // ?
-// 4
-var sentence = 'welcome to bootcamp prep';
-var lastChar = sentence[sentence.length - 1];
-console.log(lastChar); // ?
-console.log(sentence.indexOf(lastChar)); // ?
+// // 1
+// var idx = 'abcde'.indexOf('D');
+// idx = idx + 11;
+// console.log(idx); // ?
+// idx * 2;
+// console.log(idx); // ?
+// // 2
+// var num = 33;
+// var isEven = num % 2 === 0;
+// console.log(isEven); // ?
+// console.log(!isEven); // ?
+// // 3
+// var str1 = 'marker';
+// var num = 'whiteboard'.length - str1.length;
+// console.log(num); // ?
+// var str2 = 'bootcamp';
+// console.log(str2[num].toUpperCase()); // ?
+// var char = str2[num].toLowerCase(); // ?
+// console.log(char + '!'); // ?
+// // 4
+// var sentence = 'welcome to bootcamp prep';
+// var lastChar = sentence[sentence.length - 1];
+// console.log(lastChar); // ?
+// console.log(sentence.indexOf(lastChar)); // ?
 
-//CONDITIONALS
+// //CONDITIONALS
 
-// 5
-var age = 30; // try different numbers here
+// // 5
+// var age = 30; // try different numbers here
 
-if (age > 30) {
-  console.log('older than 30');
-} else {
-  console.log('younger than 30');
-}
-// 6
-var str = 'pizza'; // try different strings here
+// if (age > 30) {
+//   console.log('older than 30');
+// } else {
+//   console.log('younger than 30');
+// }
+// // 6
+// var str = 'pizza'; // try different strings here
 
-if (str.length > 10) {
-  console.log('long string');
-} else {
-  console.log('short string');
-}
-if (str[0] === 'p') {
-  console.log('starts with p');
-}
-// 7
-var num = 15; // try different numbers here
+// if (str.length > 10) {
+//   console.log('long string');
+// } else {
+//   console.log('short string');
+// }
+// if (str[0] === 'p') {
+//   console.log('starts with p');
+// }
+// // 7
+// var num = 15; // try different numbers here
 
-if (num % 3 === 0) {
-  console.log('divisible by 3');
-} else if (num % 5 === 0) {
-  console.log('divisible by 5');
-}
-// 8
-var num = 15; // try different numbers here
+// if (num % 3 === 0) {
+//   console.log('divisible by 3');
+// } else if (num % 5 === 0) {
+//   console.log('divisible by 5');
+// }
+// // 8
+// var num = 15; // try different numbers here
 
-if (num % 3 === 0) {
-  console.log('divisible by 3');
-}
-if (num % 5 === 0) {
-  console.log('divisible by 5');
-}
-// 9
-var str = 'App AcademY'; // try different strings here
+// if (num % 3 === 0) {
+//   console.log('divisible by 3');
+// }
+// if (num % 5 === 0) {
+//   console.log('divisible by 5');
+// }
+// // 9
+// var str = 'App AcademY'; // try different strings here
 
-if (str[0] === str[0].toUpperCase()) {
-  console.log('starts with a capital!');
-}
-if (str[str.length - 1] === str[str.length - 1].toUpperCase()) {
-  console.log('ends with a capital!');
-}
-// 10
-var num = -44; // try different numbers here
+// if (str[0] === str[0].toUpperCase()) {
+//   console.log('starts with a capital!');
+// }
+// if (str[str.length - 1] === str[str.length - 1].toUpperCase()) {
+//   console.log('ends with a capital!');
+// }
+// // 10
+// var num = -44; // try different numbers here
 
-if (num > 0) {
-  console.log('positive');
-} else if (num < 0) {
-  console.log('negative');
-} else {
-  console.log(0);
-}
-if (num % 2 === 0) {
-  console.log('even');
-} else {
-  console.log('odd');
-}
+// if (num > 0) {
+//   console.log('positive');
+// } else if (num < 0) {
+//   console.log('negative');
+// } else {
+//   console.log(0);
+// }
+// if (num % 2 === 0) {
+//   console.log('even');
+// } else {
+//   console.log('odd');
+// }
 
-var num = 11; // feel free to change the value of this variable
-if (num > 5) {
-  console.log('if');
-}
-var num = 5; // feel free to change the value of this variable
-if (num > 5) {
-  console.log('if');
-} else {
-  console.log('else');
-}
-var num = 0; // feel free to change the value of this variable
-if (num < 0) {
-  console.log('if');
-} else if (num > 0) {
-  console.log('else if');
-} else {
-  console.log('else');
-}
+// var num = 11; // feel free to change the value of this variable
+// if (num > 5) {
+//   console.log('if');
+// }
+// var num = 5; // feel free to change the value of this variable
+// if (num > 5) {
+//   console.log('if');
+// } else {
+//   console.log('else');
+// }
+// var num = 0; // feel free to change the value of this variable
+// if (num < 0) {
+//   console.log('if');
+// } else if (num > 0) {
+//   console.log('else if');
+// } else {
+//   console.log('else');
+// }
