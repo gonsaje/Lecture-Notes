2aa7c935c8d1c0135e9d68725709707ef930222a
gonsaje <song.jaehyun@gmail.com>
2020-11-25 19:13:46 -0500

    practice_problems

---
+++ practice_problems.js
@@ -0,0 +1,66 @@
+// We want an array, but not just any old array, an array with contents!
+
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+
+// For example, the following code will result in an array containing the numbers 0 to 4:
+
+
+
+
+
+
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+
+
+
+
+
+
+// Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+
+// Task
+// Implement a function named
+
+// generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+// generateRange(1, 10, 3) // should return array of [1,4,7,10]
+// Note
+// min < max
+// step > 0
+// the range does not HAVE to include max (depending on the step)
+
+
+
+
+
+
+
+// When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot. The petals start to fall in late April.
+
+// Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s), and it takes 80 seconds for the petal to reach the ground from a certain branch.
+
+// Write a function that receives the speed (in cm/s) of a petal as input, and returns the time it takes for that petal to reach the ground from the same branch.
+
+// Notes:
+
+// The movement of the petal is quite complicated, so in this case we can see the velocity as a constant during its falling.
+// Pay attention to the data types.
+// If the initial velocity is non-positive, the return value should be 0
+
+
+
+
+
+
+// Speed is a crucial measure of success for any aspiring warrior, so let's write a function to calculate it.
+
+// Average Speed is calculated by dividing distance by time. Given two strings as input the first of which indicates a codewarrior's distance travelled (in metres or kilometres) and the second indicating the time it takes them to travel (in seconds or minutes), return a string indicating their speed in miles per hour rounded to the nearest integer.
+
+// For the purposes of this kata one metre per second is defined as 2.23694 miles per hour.
+
+// So for example given the input values of distance & time "3km" and "5min" we should return a speed value of "22mph".
