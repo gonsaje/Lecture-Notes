973cc3fdaf6c466d85b0caf156e392180170af86
gonsaje <song.jaehyun@gmail.com>
2021-05-18 17:32:34 -0400

    init for day 2

--- w1d1_hw.js
+++ w1d1_hw.js
@@ -8,17 +8,17 @@
 
 // true && false
 
-// console.log(true && !(false || false)) // true
+// console.log(true && !(false || false)) // 
 
 // console.log(false || true) // 
 
 // 'cat' + 'dog'
 
-// console.log(2 + 'pizza'); // '2pizza'
+// console.log(2 + 'pizza'); // 
 
 // console.log(2 + '2'); // 
 
-// console.log(2.5 * 'fish'); // '2.5fish'
+// console.log(2.5 * 'fish'); // 
 
 // 5 >= 11
 
@@ -32,7 +32,7 @@
 
 // 2 > 1 || false
 
-// console.log('true' === true); // false
+// console.log('true' === true); // 
 
 // 10 % 2 === 0
 
@@ -53,50 +53,50 @@
 // 'new york'[1]
 
 var str = "san francisco";
-// console.log(str[2 * 3]) //a
+// console.log(str[2 * 3]) //
 
 var str2 = 'bootcamp'
-// console.log(str2[3].toUpperCase()) // T
-// console.log(str2[3].toUpperCase())
+// console.log(str2[3].toUpperCase()) // 
+
 // console.log(str2.indexOf('T')) // 
 
-// console.log('bootcamp'.indexOf('camp')) //4
+// console.log('bootcamp'.indexOf('camp')) //
 
-// console.log('bootcamp'.indexOf('o') > -1) // true
+// console.log('bootcamp'.indexOf('o') > -1) // 
 
 // console.log('science'.indexOf('x') === -1) // 
 
 
 
 // 1
-var idx = 'abcde'.indexOf('D'); // -1
-idx = idx + 11; // 10
-// console.log(idx); // 10
-// idx * 2; // 20
-// console.log(idx); // 10
+var idx = 'abcde'.indexOf('D'); //
+idx = idx + 11; // 
+// console.log(idx); // 
+idx * 2; // 
+// console.log(idx); // 
 
 // 2
 var num = 33;
-var isEven = num % 2 === 0; // false
-// console.log(isEven); // false
-// console.log(!isEven); // true
+var isEven = num % 2 === 0; // 
+// console.log(isEven); // 
+// console.log(!isEven); // 
 
 // 3
 var str1 = 'marker';
-var num = 'whiteboard'.length - str1.length; // 4
-// console.log(num); // 4
-// var str2 = 'bootcamp';
-// console.log(str2[num].toUpperCase()); // C
-// var char = str2[num]; // c
-// console.log(char + '!'); // c!
+var num = 'whiteboard'.length - str1.length; // 
+// console.log(num); // 
+var str2 = 'bootcamp';
+// console.log(str2[num].toUpperCase()); // 
+var char = str2[num]; // 
+// console.log(char + '!'); // 
 
 // 4 
 
 var sentence = 'welcome to bootcamp prep'; 
-var lastChar = sentence[sentence.length - 1]; // p
-// console.log(lastChar); // p
-// console.log(sentence.indexOf(lastChar)); // 18
-// console.log(sentence.length - 1)
+var lastChar = sentence[sentence.length - 1]; // 
+// console.log(lastChar); // 
+// console.log(sentence.indexOf(lastChar)); // 
+
 
 // //CONDITIONALS
 
@@ -112,7 +112,7 @@ var age = 30; // try different numbers here
 // }
 
 // 6
-// var str = 'roller'; // 
+// var str = 'rollercoaster'; // 
 
 // if (str.length > 10) {
 
